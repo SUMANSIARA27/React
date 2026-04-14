37b68e910dce79ba2c1059b6fd0f265126504c0a
SUMANSIARA27 <sumansiara27@gmail.com>
2026-04-14 10:40:13 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,13 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I,am H1 TAG"),
+  ),
+);
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
